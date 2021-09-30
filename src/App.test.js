7812422e45463acabaec32eb9be7bf4252cab46d
Norton dos Santos <nortonsantos79@gmail.com>
2021-09-30 11:28:30 -0300

    implementei alguns testes

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,32 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from '../src/components/App/index';
+import renderer from 'react-test-renderer';
+import {pickAndChoose} from '../src/constants/index';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App', () => {
+  it('renderização', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  test('snapshot', ()=>{
+    const component = renderer.create(
+      <App />
+    );
+    const tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  test('random', ()=>{
+    const value = pickAndChoose();
+    expect(value).toBeTruthy();
+  });
+
+  test('type', ()=>{
+    const value = pickAndChoose();
+    expect(typeof value).toBe("number");
+  });
 });
+
